ceeba0ab9b4c3ecbec7724a5d3bede181b6ea33d
razonyang <razonyang@gmail.com>
2020-06-18 07:36:41 +0800

    Update sqlite3store

--- stores/sqlite3store/go.mod
+++ stores/sqlite3store/go.mod
@@ -2,4 +2,8 @@ module clevergo.tech/captchas/stores/sqlite3store
 
 go 1.14
 
-require clevergo.tech/captchas/stores/dbstore v0.1.0
+require (
+	clevergo.tech/captchas v0.1.0
+	clevergo.tech/captchas/stores/dbstore v0.1.0
+	github.com/doug-martin/goqu/v9 v9.9.0
+)
--- stores/sqlite3store/store.go
+++ stores/sqlite3store/store.go
@@ -7,7 +7,10 @@ package sqlite3store
 import (
 	"database/sql"
 
+	"clevergo.tech/captchas"
 	"clevergo.tech/captchas/stores/dbstore"
+	"github.com/doug-martin/goqu/v9"
+	_ "github.com/doug-martin/goqu/v9/dialect/sqlite3"
 )
 
 // Store is a Sqlite3 store.
@@ -15,7 +18,9 @@ type Store struct {
 	*dbstore.Store
 }
 
+var _ captchas.Store = New(nil)
+
 // New returns store instance.
 func New(db *sql.DB, opts ...dbstore.Option) *Store {
-	return &Store{dbstore.New(db, dbstore.CommonDialect, opts...)}
+	return &Store{dbstore.New(db, goqu.Dialect("sqlite3"), opts...)}
 }
